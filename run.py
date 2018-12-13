ace5e69d77312f29aa4acfd4e089da83b8bca71b
Dimitar Milev <rokambol_88@abv.bg>
2018-12-13 23:38:15 +0000

    add bootstrap style and base.html using inherence method for html files

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, redirect, render_template, request 
+from flask import Flask, redirect, render_template, request, url_for
 import random
 from random import randint, choice, shuffle
 
@@ -11,11 +11,23 @@ with open("data/questions.json", "r") as question:
     riddles = json.load(question)
     
 @app.route('/', methods=["POST", "GET"])
-def home():
+def index():
     qw = riddles
     number = random.shuffle(riddles)
     #question1 = qw.pop(), question1 = question1
-    return render_template("index.html", page_title="Test",qw=qw, number=number)
+    return render_template("index.html", page_title="Home",qw=qw, number=number)
+    
+@app.route('/rules')
+def rules():
+    return render_template("rules.html", page_title="Rules")
+    
+@app.route('/history')
+def history():
+    return render_template("history.html", page_title="History")
+    
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template("leaderboard.html", page_title="Leaderboard")
      
 app.run(host=os.environ.get('IP'), port=os.environ.get('PORT'), debug=True)
 
